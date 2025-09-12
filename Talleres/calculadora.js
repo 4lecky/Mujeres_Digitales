4dd33a25f7e389bebdef568fb20d480effce86f0
4lecky <lunitaromero1221@gmail.com>
2025-09-11 20:50:50 -0500

    Calculadora

--- Talleres/calculadora.js
+++ Talleres/calculadora.js
@@ -3,20 +3,94 @@ const rl = require('readline').createInterface({
     output: process.stdout,
 });
 
-console.log('Calculadora');
-
-const numUno = await preguntar('El primer número:');
-const numDos = await preguntar('El segundo número:');
-
-const memuu = await preguntar('Elija la operación a realizar: \n 1) Sumar \n 2) Restar \n 3) Multiplicar \n 4) Dividir \n');
-
+const preguntar = (pregunta) => {
+    return new Promise((resolve) => {
+        rl.question(pregunta, (respuesta) => resolve(respuesta));
+    });
+};
 
 
-switch (key) {
-    case value:
-        
-        break;
+const menu = async () => {
 
-    default:
-        break;
+    console.log("1. Sumar");
+    console.log("2. Restar");
+    console.log("3. Multiplicar");
+    console.log("4. Dividir");
+    console.log("5. Porcentaje");
+    console.log("6. Potencia");
+    console.log("7. Promedio")
+    console.log("8. Salir");
+
+    const selección = await preguntar("Seleccione una opción:");
+
+    console.log('=== Bienvenido a la calculadora === \n Ingrese dos números:');
+    let numUno;
+    let numDos;
+
+    // Verificamos que la selección esté dentro de las opciones que requieren números, osea que default y salir no lo requieran los numeros
+    if (['1', '2', '3', '4', '5','6','7'].includes(selección)) {
+        numUno = await preguntar("Ingrese el primer numero: ");
+        numDos = await preguntar("Ingrese el segundo numero: ");
+    }
+
+    switch (selección) {
+
+        case '1':
+            console.clear();
+            console.log("Ha seleccionado sumar");
+            console.log(`El resultado es: ✅ ${Number(numUno) + Number(numDos)}`);
+            break;
+
+        case '2':
+            console.clear();
+            console.log("Ha seleccionado restar");
+            console.log(`El resultado es: ✅ ${Number(numUno) - Number(numDos)}`);
+            break;
+        case '3':
+            console.clear();
+            console.log("Ha seleccionado multiplicar");
+            console.log(`El resultado es: ✅ ${Number(numUno) * Number(numDos)}`);
+            break;
+        case '4':
+            console.clear();
+            console.log("Ha seleccionado dividir");
+            // Verificamos que el divisor no sea cero, ya que no se puede dividir por cero
+            if (Number(numDos) === 0) {
+                console.log("No se puede dividir por cero");
+            } else {
+                console.log(`El resultado es: ✅ ${Number(numUno) / Number(numDos)}`);
+            }
+            break;
+        case '5':
+            console.clear();
+            console.log("Ha seleccionado porcentaje");
+            // Tomamos el primer numero como % y el segun al que sacarle el % 
+            console.log(`${numUno}% de ${numDos} es: ✅ ${(Number(numUno) * Number(numDos)) / 100}`);
+            break;
+        case '6':
+            console.clear();
+            console.log("Ha seleccionado potencia");
+            console.log(`El resultado es: ✅ ${(Number(numUno) ** Number(numDos))}`);
+            break;
+        case '7':
+            console.clear();
+            console.log("Ha seleccionado promedio");
+            console.log(`El resultado es: ✅ ${(Number(numUno) + Number(numDos)) / 2}`);
+            break;
+        case '8':
+            console.clear();
+            console.log("Gracias por usar la calculadora");
+            rl.close();
+             return;
+        default:
+            console.clear();
+            console.log("Opción inválida, por favor seleccione una opción del 1 al 5");
+            break;
+    }
+
+    menu();
+
 }
+
+console.clear();
+menu();
